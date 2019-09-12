58dceced3a282fc7739d176cdf0fc9e3b20d3b4b
Peyton Duncan <peyton.duncan45@gmail.com>
2019-09-12 16:02:19 -0400

    Defining new functions for the schedule library

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -3,13 +3,26 @@ import requests
 
 HACKUCF_URL="https://calendar.google.com/calendar/ical/calendar%40hackucf.org/public/basic.ics"
 
-def get_latest_event():
+def get_next_event():
     # Get the calendar
     c = Calendar(requests.get(HACKUCF_URL).text)
 
     # Parse out the next event
     return list(c.timeline)[-1].begin
+
     # return list(c.timeline)
 
+def get_next_meeting():
+    return ""
+
+def get_next_workshop():
+    return ""
+
+def get_next_ops_meeting():
+    return ""
+
+def get_next_competition():
+    return ""
+
 if __name__ == "__main__":
-    print(get_latest_event())
+    print(get_next_event())
