05f3f2d6aec5b02c769e343a8166255df2dc2e43
Peyton Duncan <peyton.duncan45@gmail.com>
2019-09-11 20:12:19 -0400

    Docstring added

--- alexa/response.py
+++ alexa/response.py
@@ -1,6 +1,17 @@
 import json
 
 def plaintext_response(text, end_session=False):
+    """Prepare a response for Alexa
+    
+    Arguments:
+        text {string} -- Text you want Alexa to say
+    
+    Keyword Arguments:
+        end_session {bool} -- whether or not the shouldEndSession flag is set (default: {False})
+    
+    Returns:
+        string -- JSON ready to return to Alexa
+    """
     template = {
         "body": {
             "version": "1.0",
